095a6f783ba264b391a04a327a26e09eb09d9c97
nour-abo lila <163058773+nour-abolila@users.noreply.github.com>
2024-09-01 21:19:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let totop = document.getElementById('totop');
+totop.addEventListener('click', function () {
+    window.scrollTo({
+        
+        top: 0,
+        behavior: 'smooth'
+    })
+})
